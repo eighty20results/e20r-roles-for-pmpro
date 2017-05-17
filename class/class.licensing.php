4804dedceea6b278cd2bb03470dd1ab2e4089ea9
Thomas Sjolshagen <thomas@eighty20results.com>
2017-05-17 11:04:50 -0600

    BUG/FIX: Updated URL for the account page for license status access. BUG/FIX: Loading duplicate E20R License Settings menu entries ENHANCEMENT: Add check for previously loaded license settings menu/page

--- class/class.licensing.php
+++ class/class.licensing.php
@@ -513,13 +513,75 @@ if ( ! class_exists( 'E20R\Licensing\Licensing' ) ) {
 		 */
 		public static function add_options_page() {
 			
-			add_options_page(
+			$utils = Utilities::get_instance();
+			$utils->log("Loading the License settings page for the Roles plugin");
+			
+			// Check whether the Licensing page is already loaded or not
+			if ( false === self::is_license_page_loaded( 'e20r-licensing', true ) ) {
+				
+				add_action('admin_menu', array( self::get_instance(), 'load_license_settings_page'));
+				
+			}
+		}
+		
+		public function load_license_settings_page() {
+			
+			$handle = add_options_page(
 				__( "E20R Licensing", self::$text_domain ),
 				__( "E20R Licensing", self::$text_domain ),
 				'manage_options',
 				'e20r-licensing',
-				'E20R\Licensing\Licensing::licensing_page'
+				array( self::get_instance() , 'licensing_page' )
 			);
+			
+			error_log( "loaded handle: {$handle}");
+		}
+		/**
+		 * Check whether the Licensing page is already loaded or not
+		 *
+		 * @param string $handle
+		 * @param bool   $sub
+		 *
+		 * @return bool
+		 */
+		public static function is_license_page_loaded( $handle, $sub = false ) {
+			
+			$utils = Utilities::get_instance();
+			
+			if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+				$utils->log("Not in back-end");
+				return false;
+			}
+			
+			global $menu;
+			global $submenu;
+			
+			$check_menu = $sub ? $submenu : $menu;
+			
+			if ( empty( $check_menu ) ) {
+				$utils->log("No menu object found??");
+				return false;
+			}
+			
+			foreach ( $check_menu as $k => $item ) {
+    
+				if ( false !== $sub ) {
+					
+					foreach ( $item as $subm ) {
+						
+						if ( false !== stripos( $subm[2], $handle ) ) {
+							return true;
+						}
+					}
+				} else {
+					
+					if ( false !== stripos( $item[2], $handle ) ) {
+						return true;
+					}
+				}
+			}
+			
+			return false;
 		}
 		
 		/**
@@ -734,7 +796,7 @@ if ( ! class_exists( 'E20R\Licensing\Licensing' ) ) {
 			<?php
 			
 			$settings            = self::get_license_settings();
-			$support_account_url = apply_filters( 'e20r-license-support-account-url', 'https://eighty20results.com/login/?redirect_to=/accounts/' );
+			$support_account_url = apply_filters( 'e20r-license-support-account-url', sprintf( 'https://eighty20results.com/login/?redirect_to=%s', home_url( '/account/' ) ) );
 			
 			foreach ( $settings as $prod => $license ) {
 				
